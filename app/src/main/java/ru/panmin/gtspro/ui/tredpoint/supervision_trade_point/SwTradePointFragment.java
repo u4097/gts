1bd5d776f6aca2f9ce3e857551003feded601f66
iteriy <iteriy4500@gmail.com>
2018-07-27 13:42:58 +0300

    'init_sw_trede_point'

--- app/src/main/java/ru/panmin/gtspro/ui/tredpoint/supervision_trade_point/SwTradePointFragment.java
+++ app/src/main/java/ru/panmin/gtspro/ui/tredpoint/supervision_trade_point/SwTradePointFragment.java
@@ -1,4 +1,67 @@
 package ru.panmin.gtspro.ui.tredpoint.supervision_trade_point;
 
-public class SwTradePointFragment {
+import java.util.Objects;
+
+import javax.inject.Inject;
+
+import ru.panmin.gtspro.R;
+import ru.panmin.gtspro.ui.base.BaseActivity;
+import ru.panmin.gtspro.ui.progress.EmptyBundle;
+import ru.panmin.gtspro.ui.toolbar.ToolbarFragment;
+
+
+public class SwTradePointFragment extends ToolbarFragment implements SwTradePointMvpView{
+
+    @Inject
+    SwTradePointPresenter presenter;
+
+    @Override
+    protected void initToolbar() {
+
+    }
+
+    @Override
+    protected int getDataView() {
+        return 0;
+    }
+
+    @Override
+    protected int getLayout() {
+        return R.layout.fragment_recycler_trade_point;
+    }
+
+    @Override
+    protected EmptyBundle getEmptyBundle() {
+        return null;
+    }
+
+    @Override
+    protected void emptyButtonClick() {
+
+    }
+
+    @Override
+    protected void errorButtonClick() {
+
+    }
+
+    @Override
+    protected void inject() {
+        ((BaseActivity) Objects.requireNonNull(getActivity())).activityComponent().inject(this);
+    }
+
+    @Override
+    protected void attachView() {
+        presenter.attachView(this);
+    }
+
+    @Override
+    protected void initViews() {
+        setStateData();
+    }
+
+    @Override
+    protected void detachView() {
+        presenter.detachView();
+    }
 }
